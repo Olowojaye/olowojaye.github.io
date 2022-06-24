1d2fe220ec3248477fa25442eeff604441654151
Olowojaye <76196041+Olowojaye@users.noreply.github.com>
2022-06-24 20:52:14 +0100

    Add files via upload
    3rd design.

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
+let navBar = document.getElementById('navbar');
+
+// let leftNav = document.getElementById('left-nav');
+
+function openNav() {
+    navBar.style.width = "100%";
+}
+
+
+function closeNav() {
+    let x = window.matchMedia("(max-width: 768px)");
+    if (x.matches) {
+        navBar.style.width = "0";
+    }
+}
+
 
 /* Code for changing active link on scrolling or clicking */
-var sections = document.querySelectorAll('section');
+var sections = document.querySelectorAll('.page-section');
 var links = document.querySelectorAll('.nav-link');
 let scrHeight = screen.height;
 
-window.addEventListener('scroll', () => {
+window.onscroll = () => {
     let current = '';
     sections.forEach(section => {
         const sectionTop = section.offsetTop; //distance of section from parent
@@ -12,15 +28,34 @@ window.addEventListener('scroll', () => {
         const pageOffset = pageYOffset; //distance of current location from top end of page
         if (pageOffset + scrHeight/3 >= sectionTop) {
             current = section.getAttribute('id');
+            section.classList.add('show');
+        }
+        else {
+            section.classList.remove('show');
         }
     })
 
 links.forEach(link => {
     link.classList.remove('active');
     if (link.classList.contains(current)) {
-        link.classList.add('active')
+        link.classList.add('active');
     }
 })
 
-})
+};
 
+// function reveal() {  
+//     for (var i = 0; i < sections.length; i++) {
+//       var windowHeight = window.innerHeight;
+//       var elementTop = sections[i].getBoundingClientRect().top;
+//       var elementVisible = 150;
+  
+//       if (elementTop < windowHeight - elementVisible) {
+//         sections[i].classList.add("show");
+//       } else {
+//         sections[i].classList.remove("show");
+//       }
+//     }
+//   };
+  
+//   window.addEventListener("scroll", reveal);
